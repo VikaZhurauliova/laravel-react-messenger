c7451328706737e43be1527d0f326e1052433739
Vika Zhurauliova <122206989+VikaZhurauliova@users.noreply.github.com>
2024-07-11 12:25:17 +0300

    Create ChatLayout

---
+++ resources/js/Layouts/ChatLayout.jsx
@@ -0,0 +1,18 @@
+import {usePage} from "@inertiajs/react";
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+
+const ChatLayout = ({children}) => {
+    const page = usePage();
+    const conversations = page.props.conversations;
+    const selectedConversation = page.props.selectedConversation;
+
+    console.log('conversations', conversations)
+    console.log('selectedConversation', selectedConversation)
+    return (
+        <>
+            ChatLayout
+            <div>{children}</div>
+        </>
+    )
+}
+export default ChatLayout;
