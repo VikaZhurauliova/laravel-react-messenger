0b2b346f67c42a1551ed57debc60465b25a44b27
Vika Zhurauliova <122206989+VikaZhurauliova@users.noreply.github.com>
2024-07-19 21:09:01 +0300

    Fix Chatlayout

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,10 +1,10 @@
-import {usePage} from "@inertiajs/react";
-import {useEffect, useState} from "react";
-import {PencilSquareIcon} from "@heroicons/react/24/solid/index.js";
+import { usePage } from "@inertiajs/react";
+import { useEffect, useState } from "react";
+import { PencilSquareIcon } from "@heroicons/react/24/solid/index.js";
 import TextInput from "@/Components/TextInput.jsx";
 import ConversationItem from "@/Components/App/ConversationItem.jsx";
 
-const ChatLayout = ({children}) => {
+const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
@@ -13,39 +13,39 @@ const ChatLayout = ({children}) => {
     const [sortedConversations, setSortedConversations] = useState([]);
     const isUserOnline = (userId) => onlineUsers[userId];
 
-    console.log('conversations', conversations)
-    console.log('selectedConversation', selectedConversation)
+    console.log('conversations', conversations);
+    console.log('selectedConversation', selectedConversation);
 
     const onSearch = (ev) => {
         const search = ev.target.value.toLowerCase();
         setLocalConversations(
             conversations.filter((conversation) => {
                 return (
-                  conversation.name.toLowerCase().includes(search)
-                 );
+                    conversation.name.toLowerCase().includes(search)
+                );
             })
-        )
-    }
+        );
+    };
 
     useEffect(() => {
         setSortedConversations(
-            localConversations.sort((a,b) => {
+            localConversations.sort((a, b) => {
                 if (a.blocked_at > b.blocked_at) {
                     return a.blocked_at > b.blocked_at ? 1 : -1;
-                } else if (a.blocked_at)  {
+                } else if (a.blocked_at) {
                     return 1;
                 } else if (b.blocked_at) {
                     return -1;
                 }
 
-                if(a.last_message_date && b.last_message_date) {
+                if (a.last_message_date && b.last_message_date) {
                     return b.last_message_date.localeCompare(
                         a.last_message_date
                     );
                 } else if (a.last_message_date) {
                     return -1;
                 } else if (b.last_message_date) {
-                    return 1
+                    return 1;
                 } else {
                     return 0;
                 }
@@ -53,38 +53,38 @@ const ChatLayout = ({children}) => {
         );
     }, [localConversations]);
 
-    useEffect(()=> {
+    useEffect(() => {
         setLocalConversations(conversations);
     }, [conversations]);
 
     useEffect(() => {
         window.Echo.join('online')
             .here((users) => {
-               const onlineUsersObj = Object.fromEntries(
-                   users.map((user) => [user.id, user])
-               );
-               setOnlineUsers((prevOnlineUsers) => {
-                   return { ...prevOnlineUsers, ...onlineUsersObj };
-               });
+                const onlineUsersObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+                setOnlineUsers((prevOnlineUsers) => {
+                    return { ...prevOnlineUsers, ...onlineUsersObj };
+                });
             })
             .joining((user) => {
-               setOnlineUsers((prevOnlineUsers) => {
-                   const updatedUsers = { ...prevOnlineUsers};
-                   updatedUsers[user.id] = user;
-                   return updatedUsers;
-               });
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updatedUsers = { ...prevOnlineUsers };
+                    updatedUsers[user.id] = user;
+                    return updatedUsers;
+                });
             })
             .leaving((user) => {
                 setOnlineUsers((prevOnlineUsers) => {
-                    const updatedUsers = { ...prevOnlineUsers};
+                    const updatedUsers = { ...prevOnlineUsers };
                     delete updatedUsers[user.id];
                     return updatedUsers;
-                })
+                });
             })
             .error((error) => {
-                console.log('error', error)
-            })
-    })
+                console.log('error', error);
+            });
+    }, []);
 
     return (
         <>
@@ -100,8 +100,7 @@ const ChatLayout = ({children}) => {
                             className="tooltip tooltip-left"
                             data-tip="Create new Group"
                         >
-                            <button className="text-gray-400 hover:text-gray-200"
-                            >
+                            <button className="text-gray-400 hover:text-gray-200">
                                 <PencilSquareIcon className="w-4 h-4 inline-block ml-2"/>
                             </button>
                         </div>
@@ -115,11 +114,11 @@ const ChatLayout = ({children}) => {
                     </div>
                     <div className="flex-1 overflow-auto">
                         {sortedConversations &&
-                            sortedConversations.map((conversation) => (
+                            sortedConversations.map((conversation, index) => (
                                 <ConversationItem
                                     key={`${
                                         conversation.is_group ? "group_" : "user_"
-                                    }${conversation.id}`}
+                                    }${conversation.id}_${index}`}
                                     conversation={conversation}
                                     online={!!isUserOnline(conversation.id)}
                                 />
@@ -131,6 +130,7 @@ const ChatLayout = ({children}) => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
+
 export default ChatLayout;
