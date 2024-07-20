378df10026c69db90f78eddf8daede00156f1a98
Vika Zhurauliova <122206989+VikaZhurauliova@users.noreply.github.com>
2024-07-20 20:43:20 +0300

    Render Messages

--- resources/js/helpers.jsx
+++ resources/js/helpers.jsx
@@ -1,3 +1,5 @@
+import {isAndroid} from "@headlessui/react/dist/utils/platform.js";
+
 export const formatMessageDateLong = (date) => {
     const now = new Date();
     const inputDate = new Date(date);
@@ -66,3 +68,50 @@ export const isYesterday = (date) => {
       date.getFullYear() === yesterday.getFullYear()
     );
 };
+
+export const isImage = (attachment) => {
+    let mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "image";
+};
+export const isVideo = (attachment) => {
+    let mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "video";
+}
+
+export const isAudio = (attachment) => {
+    let mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "audio";
+}
+export const isPDF = (attachment) => {
+    let mime = attachment.mime || attachment.type;
+    return mime === "application/pdf";
+}
+
+export const isPreviewable = (attachment) => {
+    return (
+        isImage(attachment) ||
+        isVideo(attachment) ||
+        isAudio(attachment) ||
+        isPDF(attachment)
+    );
+};
+
+export const formatBytes = (bytes, decimals = 2) => {
+    if (bytes === 0) return "0 Bytes";
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB"];
+
+    let i = 0;
+    let size = bytes;
+    while (size >= k) {
+        size /= k;
+        i++;
+    }
+
+    return parseFloat(size.toFixed(dm)) + " " + sizes[i];
+}
