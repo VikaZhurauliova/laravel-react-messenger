d1074a88d34b0d023803439d06716ca98ce661c2
Vika Zhurauliova <122206989+VikaZhurauliova@users.noreply.github.com>
2024-08-21 21:00:17 +0300

    Load older messages on scroll

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { PencilSquareIcon } from "@heroicons/react/24/solid/index.js";
 import TextInput from "@/Components/TextInput.jsx";
 import ConversationItem from "@/Components/App/ConversationItem.jsx";
+import {useEventBus} from "@/EventBus.jsx";
 
 const ChatLayout = ({ children }) => {
     const page = usePage();
@@ -11,6 +12,8 @@ const ChatLayout = ({ children }) => {
     const [onlineUsers, setOnlineUsers] = useState({});
     const [localConversations, setLocalConversations] = useState([]);
     const [sortedConversations, setSortedConversations] = useState([]);
+    const { on } = useEventBus();
+
     const isUserOnline = (userId) => onlineUsers[userId];
 
     const onSearch = (ev) => {
@@ -23,6 +26,40 @@ const ChatLayout = ({ children }) => {
             })
         );
     };
+
+    const messageCreated = (message) => {
+        setLocalConversations((oldUsers) => {
+            return oldUsers.map((u) => {
+                if(
+                    message.receiver_id &&
+                    !u.is_group &&
+                    (u.id == message.sender_id || u.id == message.receiver_id)
+                ) {
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+
+                if (
+                    message.group_id &&
+                    u.is_group &&
+                    u.id == message.group_id
+                ) {
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+                return u;
+            });
+        });
+    };
+
+    useEffect(() => {
+        const offCreated = on("message.created", messageCreated);
+        return () => {
+            offCreated()
+        }
+    }, [on]);
 
     useEffect(() => {
         setSortedConversations(
