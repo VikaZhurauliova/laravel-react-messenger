3a0a5aabc3bf181bb494fee6c2b873b0d93bb2a5
Vika Zhurauliova <122206989+VikaZhurauliova@users.noreply.github.com>
2024-08-28 15:59:01 +0300

    Toast notification

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -14,6 +14,7 @@ import { isAudio, isImage } from "@/helpers.jsx";
 import AttachmentPreview from "@/Components/App/AttachmentPreview.jsx";
 import CustomAudioPlayer from "@/Components/App/CustomAudioPlayer.jsx";
 import AudioRecoder from "@/Components/App/AudioRecoder.jsx";
+import {useEventBus} from "@/EventBus.jsx";
 
 const MessageInput = ({ conversation = null}) => {
     const [newMessage, setNewMessage] = useState("");
@@ -21,7 +22,7 @@ const MessageInput = ({ conversation = null}) => {
     const [messageSending, setMessageSending] = useState(false);
     const [chosenFiles, setChosenFiles] = useState([]);
     const [uploadProgress, setUploadProgress] = useState(0);
-
+    const { emit } = useEventBus();
     const onFileChange = (ev) => {
         const files = ev.target.files;
 
@@ -40,6 +41,8 @@ const MessageInput = ({ conversation = null}) => {
     };
 
     const onSendClick = () => {
+        emit("toast.show", "Message sent successfully");
+
         if(messageSending) {
             return;
         }
