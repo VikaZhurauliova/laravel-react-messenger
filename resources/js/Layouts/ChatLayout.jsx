44d28e433518d9056c3481be38220d3bdc607b57
Vika Zhurauliova <122206989+VikaZhurauliova@users.noreply.github.com>
2024-09-05 19:34:00 +0300

    Deleting Messages

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -54,10 +54,19 @@ const ChatLayout = ({ children }) => {
         });
     };
 
+    const messageDeleted = ({ prevMessage}) => {
+        if (!prevMessage) {
+            return;
+        }
+
+        messageCreated(prevMessage);
+    }
     useEffect(() => {
         const offCreated = on("message.created", messageCreated);
+        const offDeleted = on("message.deleted", messageDeleted);
         return () => {
-            offCreated()
+            offCreated();
+            offDeleted();
         }
     }, [on]);
 
